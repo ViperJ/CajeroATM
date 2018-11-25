d28e35497d850dc55f5fde95ad770de2b60326f3
Francis León <francistheviper@gmail.com>
2018-11-25 12:14:00 -0400

    nuevos cambios

--- SistemaATM/vistas/TableroNumerico.Designer.cs
+++ SistemaATM/vistas/TableroNumerico.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             this.button15 = new System.Windows.Forms.Button();
-            this.button14 = new System.Windows.Forms.Button();
+            this.numCero_btn = new System.Windows.Forms.Button();
             this.button13 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
+            this.aceptar_btn = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.numeroUno_btn = new System.Windows.Forms.Button();
+            this.borrar_btn = new System.Windows.Forms.Button();
+            this.cancelar_btn = new System.Windows.Forms.Button();
+            this.numNueve_btn = new System.Windows.Forms.Button();
+            this.numOcho_btn = new System.Windows.Forms.Button();
+            this.numSiete_btn = new System.Windows.Forms.Button();
+            this.numSeis_btn = new System.Windows.Forms.Button();
+            this.numCinco_btn = new System.Windows.Forms.Button();
+            this.numCuatro_btn = new System.Windows.Forms.Button();
+            this.numTres_btn = new System.Windows.Forms.Button();
+            this.numDos_btn = new System.Windows.Forms.Button();
+            this.numUno_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button15
@@ -54,14 +54,15 @@
             this.button15.TabIndex = 31;
             this.button15.UseVisualStyleBackColor = true;
             // 
-            // button14
+            // numCero_btn
             // 
-            this.button14.Location = new System.Drawing.Point(93, 151);
-            this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(61, 40);
-            this.button14.TabIndex = 30;
-            this.button14.Text = "0";
-            this.button14.UseVisualStyleBackColor = true;
+            this.numCero_btn.Location = new System.Drawing.Point(93, 151);
+            this.numCero_btn.Name = "numCero_btn";
+            this.numCero_btn.Size = new System.Drawing.Size(61, 40);
+            this.numCero_btn.TabIndex = 30;
+            this.numCero_btn.Text = "0";
+            this.numCero_btn.UseVisualStyleBackColor = true;
+            this.numCero_btn.Click += new System.EventHandler(this.numCero_btn_Click);
             // 
             // button13
             // 
@@ -71,14 +72,14 @@
             this.button13.TabIndex = 29;
             this.button13.UseVisualStyleBackColor = true;
             // 
-            // button9
+            // aceptar_btn
             // 
-            this.button9.Location = new System.Drawing.Point(256, 151);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(114, 40);
-            this.button9.TabIndex = 28;
-            this.button9.Text = "Aceptar";
-            this.button9.UseVisualStyleBackColor = true;
+            this.aceptar_btn.Location = new System.Drawing.Point(256, 151);
+            this.aceptar_btn.Name = "aceptar_btn";
+            this.aceptar_btn.Size = new System.Drawing.Size(114, 40);
+            this.aceptar_btn.TabIndex = 28;
+            this.aceptar_btn.Text = "Aceptar";
+            this.aceptar_btn.UseVisualStyleBackColor = true;
             // 
             // button12
             // 
@@ -88,125 +89,134 @@
             this.button12.TabIndex = 27;
             this.button12.UseVisualStyleBackColor = true;
             // 
-            // button11
+            // borrar_btn
             // 
-            this.button11.Location = new System.Drawing.Point(256, 59);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(114, 40);
-            this.button11.TabIndex = 26;
-            this.button11.Text = "Borrar";
-            this.button11.UseVisualStyleBackColor = true;
+            this.borrar_btn.Location = new System.Drawing.Point(256, 59);
+            this.borrar_btn.Name = "borrar_btn";
+            this.borrar_btn.Size = new System.Drawing.Size(114, 40);
+            this.borrar_btn.TabIndex = 26;
+            this.borrar_btn.Text = "Borrar";
+            this.borrar_btn.UseVisualStyleBackColor = true;
             // 
-            // button10
+            // cancelar_btn
             // 
-            this.button10.Location = new System.Drawing.Point(256, 13);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(114, 40);
-            this.button10.TabIndex = 21;
-            this.button10.Text = "Cancelar";
-            this.button10.UseVisualStyleBackColor = true;
+            this.cancelar_btn.Location = new System.Drawing.Point(256, 13);
+            this.cancelar_btn.Name = "cancelar_btn";
+            this.cancelar_btn.Size = new System.Drawing.Size(114, 40);
+            this.cancelar_btn.TabIndex = 21;
+            this.cancelar_btn.Text = "Cancelar";
+            this.cancelar_btn.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // numNueve_btn
             // 
-            this.button8.Location = new System.Drawing.Point(171, 105);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(61, 40);
-            this.button8.TabIndex = 25;
-            this.button8.Text = "9";
-            this.button8.UseVisualStyleBackColor = true;
+            this.numNueve_btn.Location = new System.Drawing.Point(171, 105);
+            this.numNueve_btn.Name = "numNueve_btn";
+            this.numNueve_btn.Size = new System.Drawing.Size(61, 40);
+            this.numNueve_btn.TabIndex = 25;
+            this.numNueve_btn.Text = "9";
+            this.numNueve_btn.UseVisualStyleBackColor = true;
+            this.numNueve_btn.Click += new System.EventHandler(this.numNueve_btn_Click);
             // 
-            // button7
+            // numOcho_btn
             // 
-            this.button7.Location = new System.Drawing.Point(93, 105);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(61, 40);
-            this.button7.TabIndex = 24;
-            this.button7.Text = "8";
-            this.button7.UseVisualStyleBackColor = true;
+            this.numOcho_btn.Location = new System.Drawing.Point(93, 105);
+            this.numOcho_btn.Name = "numOcho_btn";
+            this.numOcho_btn.Size = new System.Drawing.Size(61, 40);
+            this.numOcho_btn.TabIndex = 24;
+            this.numOcho_btn.Text = "8";
+            this.numOcho_btn.UseVisualStyleBackColor = true;
+            this.numOcho_btn.Click += new System.EventHandler(this.numOcho_btn_Click);
             // 
-            // button6
+            // numSiete_btn
             // 
-            this.button6.Location = new System.Drawing.Point(16, 105);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(61, 40);
-            this.button6.TabIndex = 23;
-            this.button6.Text = "7";
-            this.button6.UseVisualStyleBackColor = true;
+            this.numSiete_btn.Location = new System.Drawing.Point(16, 105);
+            this.numSiete_btn.Name = "numSiete_btn";
+            this.numSiete_btn.Size = new System.Drawing.Size(61, 40);
+            this.numSiete_btn.TabIndex = 23;
+            this.numSiete_btn.Text = "7";
+            this.numSiete_btn.UseVisualStyleBackColor = true;
+            this.numSiete_btn.Click += new System.EventHandler(this.numSiete_btn_Click);
             // 
-            // button5
+            // numSeis_btn
             // 
-            this.button5.Location = new System.Drawing.Point(171, 59);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(61, 40);
-            this.button5.TabIndex = 22;
-            this.button5.Text = "6";
-            this.button5.UseVisualStyleBackColor = true;
+            this.numSeis_btn.Location = new System.Drawing.Point(171, 59);
+            this.numSeis_btn.Name = "numSeis_btn";
+            this.numSeis_btn.Size = new System.Drawing.Size(61, 40);
+            this.numSeis_btn.TabIndex = 22;
+            this.numSeis_btn.Text = "6";
+            this.numSeis_btn.UseVisualStyleBackColor = true;
+            this.numSeis_btn.Click += new System.EventHandler(this.numSeis_btn_Click);
             // 
-            // button4
+            // numCinco_btn
             // 
-            this.button4.Location = new System.Drawing.Point(93, 59);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(61, 40);
-            this.button4.TabIndex = 20;
-            this.button4.Text = "5";
-            this.button4.UseVisualStyleBackColor = true;
+            this.numCinco_btn.Location = new System.Drawing.Point(93, 59);
+            this.numCinco_btn.Name = "numCinco_btn";
+            this.numCinco_btn.Size = new System.Drawing.Size(61, 40);
+            this.numCinco_btn.TabIndex = 20;
+            this.numCinco_btn.Text = "5";
+            this.numCinco_btn.UseVisualStyleBackColor = true;
+            this.numCinco_btn.Click += new System.EventHandler(this.numCinco_btn_Click);
             // 
-            // button3
+            // numCuatro_btn
             // 
-            this.button3.Location = new System.Drawing.Point(16, 59);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(61, 40);
-            this.button3.TabIndex = 19;
-            this.button3.Text = "4";
-            this.button3.UseVisualStyleBackColor = true;
+            this.numCuatro_btn.Location = new System.Drawing.Point(16, 59);
+            this.numCuatro_btn.Name = "numCuatro_btn";
+            this.numCuatro_btn.Size = new System.Drawing.Size(61, 40);
+            this.numCuatro_btn.TabIndex = 19;
+            this.numCuatro_btn.Text = "4";
+            this.numCuatro_btn.UseVisualStyleBackColor = true;
+            this.numCuatro_btn.Click += new System.EventHandler(this.numCuatro_btn_Click);
             // 
-            // button2
+            // numTres_btn
             // 
-            this.button2.Location = new System.Drawing.Point(171, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(61, 40);
-            this.button2.TabIndex = 18;
-            this.button2.Text = "3";
-            this.button2.UseVisualStyleBackColor = true;
+            this.numTres_btn.Location = new System.Drawing.Point(171, 13);
+            this.numTres_btn.Name = "numTres_btn";
+            this.numTres_btn.Size = new System.Drawing.Size(61, 40);
+            this.numTres_btn.TabIndex = 18;
+            this.numTres_btn.Text = "3";
+            this.numTres_btn.UseVisualStyleBackColor = true;
+            this.numTres_btn.Click += new System.EventHandler(this.numTres_btn_Click);
             // 
-            // button1
+            // numDos_btn
             // 
-            this.button1.Location = new System.Drawing.Point(93, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 40);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "2";
-            this.button1.UseVisualStyleBackColor = true;
+            this.numDos_btn.Location = new System.Drawing.Point(93, 13);
+            this.numDos_btn.Name = "numDos_btn";
+            this.numDos_btn.Size = new System.Drawing.Size(61, 40);
+            this.numDos_btn.TabIndex = 17;
+            this.numDos_btn.Text = "2";
+            this.numDos_btn.UseVisualStyleBackColor = true;
+            this.numDos_btn.Click += new System.EventHandler(this.numeroDos_btn_Click);
             // 
-            // numeroUno_btn
+            // numUno_btn
             // 
-            this.numeroUno_btn.Location = new System.Drawing.Point(16, 13);
-            this.numeroUno_btn.Name = "numeroUno_btn";
-            this.numeroUno_btn.Size = new System.Drawing.Size(61, 40);
-            this.numeroUno_btn.TabIndex = 16;
-            this.numeroUno_btn.Text = "1";
-            this.numeroUno_btn.UseVisualStyleBackColor = true;
+            this.numUno_btn.Location = new System.Drawing.Point(16, 13);
+            this.numUno_btn.Name = "numUno_btn";
+            this.numUno_btn.Size = new System.Drawing.Size(61, 40);
+            this.numUno_btn.TabIndex = 16;
+            this.numUno_btn.Text = "1";
+            this.numUno_btn.UseVisualStyleBackColor = true;
+            this.numUno_btn.Click += new System.EventHandler(this.numeroUno_btn_Click);
             // 
             // TableroNumerico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.button15);
-            this.Controls.Add(this.button14);
+            this.Controls.Add(this.numCero_btn);
             this.Controls.Add(this.button13);
-            this.Controls.Add(this.button9);
+            this.Controls.Add(this.aceptar_btn);
             this.Controls.Add(this.button12);
-            this.Controls.Add(this.button11);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numeroUno_btn);
+            this.Controls.Add(this.borrar_btn);
+            this.Controls.Add(this.cancelar_btn);
+            this.Controls.Add(this.numNueve_btn);
+            this.Controls.Add(this.numOcho_btn);
+            this.Controls.Add(this.numSiete_btn);
+            this.Controls.Add(this.numSeis_btn);
+            this.Controls.Add(this.numCinco_btn);
+            this.Controls.Add(this.numCuatro_btn);
+            this.Controls.Add(this.numTres_btn);
+            this.Controls.Add(this.numDos_btn);
+            this.Controls.Add(this.numUno_btn);
             this.Name = "TableroNumerico";
             this.Size = new System.Drawing.Size(388, 216);
             this.ResumeLayout(false);
@@ -216,20 +226,20 @@
         #endregion
 
         private System.Windows.Forms.Button button15;
-        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button numCero_btn;
         private System.Windows.Forms.Button button13;
-        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button aceptar_btn;
         private System.Windows.Forms.Button button12;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button numeroUno_btn;
+        private System.Windows.Forms.Button borrar_btn;
+        private System.Windows.Forms.Button cancelar_btn;
+        private System.Windows.Forms.Button numNueve_btn;
+        private System.Windows.Forms.Button numOcho_btn;
+        private System.Windows.Forms.Button numSiete_btn;
+        private System.Windows.Forms.Button numSeis_btn;
+        private System.Windows.Forms.Button numCinco_btn;
+        private System.Windows.Forms.Button numCuatro_btn;
+        private System.Windows.Forms.Button numTres_btn;
+        private System.Windows.Forms.Button numDos_btn;
+        private System.Windows.Forms.Button numUno_btn;
     }
 }
--- SistemaATM/vistas/TableroNumerico.cs
+++ SistemaATM/vistas/TableroNumerico.cs
@@ -12,9 +12,73 @@ namespace SistemaATM.vistas
 {
     public partial class TableroNumerico : UserControl
     {
+        private TextBox textBox;
+
         public TableroNumerico()
         {
             InitializeComponent();
+        }
+
+        public void obtenerTextBox(TextBox t)
+        {
+            textBox = t;
+        }
+
+        private void escribirTextBox(string texto)
+        {
+            string nuevoTexto = string.Concat(textBox.Text, texto);
+            textBox.Text = nuevoTexto;
+        }
+
+        private void numeroUno_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("1");
+        }
+
+        private void numeroDos_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("2");
+        }
+
+        private void numTres_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("3");
+        }
+
+        private void numCuatro_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("4");
+        }
+
+        private void numCinco_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("5");
+        }
+
+        private void numSeis_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("6");
+            
+        }
+
+        private void numSiete_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("7");
+        }
+
+        private void numOcho_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("8");
+        }
+
+        private void numNueve_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("9");
+        }
+
+        private void numCero_btn_Click(object sender, EventArgs e)
+        {
+            escribirTextBox("0");
         }
     }
 }
