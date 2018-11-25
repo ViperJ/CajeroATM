c5fe15441b1e2b2e78f83035d030cb28abe8bbaa
Francis León <francistheviper@gmail.com>
2018-11-25 12:12:08 -0400

    Agrega valicacion de tarjeta de credito

--- SistemaATM/vistas/Pantalla1.cs
+++ SistemaATM/vistas/Pantalla1.cs
@@ -12,7 +12,8 @@ namespace SistemaATM
 {
     public partial class Pantalla1 : Form
     {
-        string a;
+        //private AyudanteValidacion ayudValidacion;
+        private bool caracterInvalidol;
 
         public Pantalla1()
         {
@@ -32,6 +33,34 @@ namespace SistemaATM
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void tableroBotones_pnl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tarjetaCredito_txb_TextChanged(object sender, EventArgs e)
+        {
+            if (caracterInvalidol == true)
+            {
+                caracterInvalidol = false;
+                string texto = tarjetaCredito_txb.Text;
+                if (texto.Length > 0)
+                    tarjetaCredito_txb.Text = texto.Remove(texto.Length - 1);
+                MessageBox.Show(this, "El campo Tarjeta de Credito solo puede contener valores numericos!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tableroNumerico1_Load(object sender, EventArgs e)
+        {
+            tableroNumerico1.obtenerTextBox(tarjetaCredito_txb);
+        }
+
+        private void tarjetaCredito_txb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!AyudanteValidacion.verificarCaracterEsValido(e.KeyChar))
+                caracterInvalidol = true;
         }
     }
 }
